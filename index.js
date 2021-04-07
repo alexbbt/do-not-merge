2f81bc936a0005e89982f733417027f5b5403b53
Alexander Bell-Towne <alexander@bell-towne.com>
2021-04-07 00:16:42 -0400

    Expand WIP and DNM to include more strings and use labels

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const { Octokit } = require("@octokit/action");
 const payload = require(process.env.GITHUB_EVENT_PATH);
 
-const isWip = /\bwip\b/i.test(payload.pull_request.title);
+function checkStringIsWIP(str) {
+  return /\b(?:wip|work[ _]in[ _]progress|dnm|do[ _]not[ _]merge)\b/i.test(str);
+}
+
+function checkWIP(pull_request) {
+  if (checkStringIsWIP(pull_request.title)) {
+    return true;
+  }
+
+  const hasWIPLabel = pull_request.labels.some((label) => {
+    return checkStringIsWIP(label.name) || checkStringIsWIP(label.description);
+  })
+  if (hasWIPLabel) {
+    return true;
+  }
+
+  if (checkStringIsWIP(pull_request.body)) {
+    return true;
+  }
+
+  return false;
+}
+
+const isWip = checkWIP(payload.pull_request);
 const octokit = new Octokit();
 
 // https://developer.github.com/v3/repos/statuses/#create-a-status
@@ -11,7 +34,7 @@ octokit
     repo: payload.repository.name,
     sha: payload.pull_request.head.sha,
     state: isWip ? "pending" : "success",
-    target_url: "https://github.com/wip/action",
+    target_url: "https://github.com/alexbbt/do-not-merge",
     description: isWip ? "work in progress" : "ready for review",
     context: "WIP (action)",
   })
